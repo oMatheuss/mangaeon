5e57c8d5ef14bf792981370dc10b05b83e88ac82
Matheus <matheussmoura@outlook.com>
2023-07-20 14:56:33 -0300

    fix: added key to nav items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -33,7 +33,7 @@ export const Navbar = () => {
         >
           <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-inherit dark:bg-gray-800 md:dark:bg-inherit dark:border-gray-700'>
             {links.map((l) => (
-              <li>
+              <li key={l.to}>
                 <NavLink
                   to={l.to}
                   className={({ isActive }) =>
