2201e66f8cba8e12a23d2554ba46d4973dc3e024
Matheus <matheussmoura@outlook.com>
2023-07-20 14:55:25 -0300

    fix: align nav items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -38,8 +38,8 @@ export const Navbar = () => {
                   to={l.to}
                   className={({ isActive }) =>
                     isActive
-                      ? 'flex items-center py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
-                      : 'flex items-center py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                      ? 'flex items-center pt-2 pb-1 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+                      : 'flex items-center pt-2 pb-1 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                   }
                   aria-current='page'
                 >
