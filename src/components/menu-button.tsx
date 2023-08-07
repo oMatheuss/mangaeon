e1a3d9f8374a3b4d69b62b140f4bfd6591e39a0c
Matheus <matheussmoura@outlook.com>
2023-08-07 10:05:50 -0300

    minor ui change

--- src/components/menu-button.tsx
+++ src/components/menu-button.tsx
@@ -1,22 +1,9 @@
-interface MenuButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
+import { LucideProps, Menu, X } from 'lucide-react';
+
+interface MenuButtonProps extends LucideProps {
   active: boolean;
 }
 
 export const MenuButton = ({ active, ...rest }: MenuButtonProps) => {
-  return (
-    <button {...rest}>
-      <div
-        data-active={active}
-        className='relative group w-full h-full flex flex-col justify-around'
-      >
-        <span className='group-data-[active=true]:absolute transition-transform transform w-full h-[2px] rounded bg-current group-data-[active=true]:rotate-45'></span>
-        <span className='transition-opacity transform w-full h-[2px] rounded bg-current group-data-[active=true]:opacity-0'></span>
-        <span className='group-data-[active=true]:absolute transition-transform transform w-full h-[2px] rounded bg-current group-data-[active=true]:-rotate-45'></span>
-      </div>
-    </button>
-  );
+  return <>{active ? <X {...rest} /> : <Menu {...rest} />}</>;
 };
