b632da30ea111ba6666011a670498d33ebe96a3c
Matheus <matheussmoura@outlook.com>
2023-08-31 12:25:06 -0300

    added drop shadow to featured container (initial)

--- src/components/featured-scroll.tsx
+++ src/components/featured-scroll.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { type ErrorResponse, toErrorReponse } from '@/lib/utils';
 import type { FeaturedResponse } from '@/types/featured';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
 
 const fetchFeatured = async (): Promise<FeaturedResponse> => {
   const res = await fetch('/api/home/getFeaturedSeries.json');
@@ -23,8 +24,40 @@ export const FeaturedScroll = () => {
 
   const featured = featuredQuery.data?.featured ?? [];
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const observerCallback: IntersectionObserverCallback = (entries) => {
+    const firstElement = entries[0].target as HTMLDivElement;
+    const targetColor = firstElement.style.backgroundColor;
+
+    if (containerRef.current) {
+      containerRef.current.style.setProperty('--featured-shadow', targetColor);
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(observerCallback, {
+      root: containerRef.current,
+      rootMargin: '0px',
+      threshold: 1.0,
+    });
+
+    if (containerRef.current) {
+      containerRef.current.childNodes.forEach((child) => {
+        observer.observe(child as HTMLDivElement);
+      });
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [featured]);
+
   return (
-    <div className='relative w-full h-64 flex snap-x snap-mandatory overflow-x-auto bg-light dark:bg-dark border border-light-b dark:border-dark-b rounded shadow'>
+    <div
+      ref={containerRef}
+      className='relative w-full h-64 flex snap-x snap-mandatory overflow-x-auto bg-light dark:bg-dark border border-light-b dark:border-dark-b rounded transition-[filter] duration-500 drop-shadow-[0px_0px_30px_var(--featured-shadow)]'
+    >
       {featured?.map((val) => (
         <FeaturedCard key={val.id_chapter} item={val} />
       ))}
@@ -38,34 +71,32 @@ interface FeaturedCardProps {
 
 const FeaturedCard = ({ item }: FeaturedCardProps) => {
   return (
-    <div className='w-full snap-center shrink-0'>
-      <div
-        className='relative h-full shrink-0 w-full text-slate-100 overflow-hidden shadow-inner shadow-black/50'
-        style={{
-          backgroundImage:
-            'repeating-linear-gradient(45deg, transparent, transparent 35px, rgba(255,255,255,.1) 35px, rgba(255,255,255,.1) 70px)',
-          backgroundColor: `#${item.hex_color || 'acacac'}`,
-        }}
+    <div
+      className='snap-center relative h-full w-full shrink-0 text-slate-100 overflow-hidden shadow-inner shadow-black/50'
+      style={{
+        backgroundImage:
+          'repeating-linear-gradient(45deg, transparent, transparent 35px, rgba(255,255,255,.1) 35px, rgba(255,255,255,.1) 70px)',
+        backgroundColor: `#${item.hex_color || 'acacac'}`,
+      }}
+    >
+      <Link
+        to={item.link}
+        className='min-w-fit select-text focus:outline outline-1 outline-indigo-600 -outline-offset-1'
       >
-        <Link
-          to={item.link}
-          className='min-w-fit select-text focus:outline outline-1 outline-indigo-600 -outline-offset-1'
-        >
-          <img
-            className='h-full w-full object-contain object-bottom'
-            src={item.featured_image}
-            alt={item.series_name}
-          />
+        <img
+          className='h-full w-full object-contain object-bottom'
+          src={item.featured_image}
+          alt={item.series_name}
+        />
+      </Link>
+      <div className='absolute top-0 flex flex-col px-6 py-4 pointer-events-none'>
+        <Link className='font-bold text-xl hover:underline' to={item.link}>
+          {item.series_name}
         </Link>
-        <div className='absolute top-0 flex flex-col px-6 py-4 pointer-events-none'>
-          <Link className='font-bold text-xl hover:underline' to={item.link}>
-            {item.series_name}
-          </Link>
-          <span className='text-sm'>Capítulo {item.chapter.number}</span>
-          <span className='text-sm'>
-            {new Date(item.date).toLocaleDateString()}
-          </span>
-        </div>
+        <span className='text-sm'>Capítulo {item.chapter.number}</span>
+        <span className='text-sm'>
+          {new Date(item.date).toLocaleDateString()}
+        </span>
       </div>
     </div>
   );
