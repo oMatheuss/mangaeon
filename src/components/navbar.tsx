c7fc0e3d28ace07f8ae374fa375ae6b0db28ada2
Matheus <matheussmoura@outlook.com>
2023-09-10 18:08:30 -0300

    fix light mode hover

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -112,7 +112,7 @@ export const Navbar = () => {
               <li key={l.to}>
                 <Link
                   href={l.to}
-                  className='flex items-center py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-dark-b dark:hover:text-white md:dark:hover:bg-transparent'
+                  className='flex items-center py-2 px-3 text-gray-900 rounded hover:bg-light-b md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-dark-b dark:hover:text-white md:dark:hover:bg-transparent'
                   aria-current='page'
                 >
                   <l.icon className='mr-2' />
