091bd56f58db926a85db05f49df1ae154d6ee752
Matheus <matheussmoura@outlook.com>
2023-10-01 13:43:35 -0300

    moved reverse proxy to nextjs rewrites

--- next.config.js
+++ next.config.js
@@ -6,4 +6,20 @@ export default {
     minimumCacheTTL: 86400,
     formats: ['image/avif', 'image/webp'],
   },
+  rewrites: async () => {
+    /**
+     * Attention!!!
+     * In prod, remove this if block or set a reverse proxy like nginx
+     */
+    return [
+      {
+        source: '/mangadex/secure/:origin/:path*',
+        destination: 'https://:origin/:path*',
+      },
+      {
+        source: '/mangadex/insecure/:origin/:path*',
+        destination: 'http://:origin/:path*',
+      },
+    ];
+  },
 };
