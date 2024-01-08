8002a51b478c790a40df7f7e7319fa6439bb0b4b
Matheus <matheussmoura@outlook.com>
2024-01-08 14:21:16 -0300

    improved featured card style

--- src/components/featured-card.tsx
+++ src/components/featured-card.tsx
@@ -6,23 +6,20 @@ interface FeaturedCardProps {
   item: HighLights;
 }
 
-const background =
-  'linear-gradient(to bottom, oklch(var(--b1) / 0.2), oklch(var(--b1)) )';
-
 export const FeaturedCard = ({ item }: FeaturedCardProps) => {
   const link = `/manga/${item.id}`;
 
   return (
     <li className='relative flex sm:justify-center items-center snap-center h-full w-full shrink-0'>
-      <div className='absolute w-full h-full z-[1]' style={{ background }} />
+      <div className='absolute w-full h-full z-[1] bg-gradient-to-b from-base-100/20 to-base-100' />
       <Image
         src={item.cover}
         alt={`Imagem de ${item.title}`}
-        className='object-cover absolute w-full h-full z-[0]'
+        className='object-cover absolute w-full h-full z-[0] md:blur-sm'
         fill
       />
 
-      <div className='flex sm:justify-center max-w-[90%] h-[90%]'>
+      <div className='flex w-[90%] h-[90%]'>
         <Link
           href={link}
           className='relative hidden sm:flex aspect-[2/3] w-auto h-full z-10 self-center'
@@ -35,7 +32,7 @@ export const FeaturedCard = ({ item }: FeaturedCardProps) => {
             height={288}
           />
         </Link>
-        <div className='text-base-content flex flex-col justify-center text-left p-2 sm:px-6 sm:py-4 z-10'>
+        <div className='text-base-content flex flex-col justify-end text-left p-2 sm:px-6 sm:py-4 z-10'>
           <Link
             className='font-extrabold text-3xl hover:underline line-clamp-3'
             href={link}
