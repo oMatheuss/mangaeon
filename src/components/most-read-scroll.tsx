04b6b45fc7d04516059c4cf3043ccc2e345b777c
Matheus <matheussmoura@outlook.com>
2024-01-21 14:20:55 -0300

    added more responsiviness

--- src/components/most-read-scroll.tsx
+++ src/components/most-read-scroll.tsx
@@ -33,18 +33,18 @@ const MostReadCard = ({ item }: MostReadCardProps) => {
   return (
     <li className='flex flex-col items-center overflow-hidden rounded-lg shadow-lg bg-base-200 border border-base-content/20'>
       <Link href={link}>
-        <div className='overflow-hidden dark:shadow-dark-b shadow-md'>
+        <div className='overflow-hidden rounded-b-lg'>
           <Image
             src={item.cover}
             alt={`Image de ${item.title}.`}
-            className='w-40 h-60 object-cover object-center transition-transform hover:scale-110'
+            className='w-32 h-48 sm:w-40 sm:h-60 object-cover object-center transition-transform hover:scale-110'
             loading='lazy'
             width={160}
             height={240}
           />
         </div>
       </Link>
-      <div className='flex items-center h-16 w-32 px-2'>
+      <div className='flex items-center h-16 w-32 sm:w-40 px-2'>
         <Link
           href={link}
           className='line-clamp-3 w-full text-xs text-center font-bold tracking-wide hover:text-primary'
@@ -69,8 +69,8 @@ const MostReadFocusCard = ({ item }: MostReadFocusCardProps) => {
           <Image
             src={item.cover}
             alt={`Image de ${item.title}.`}
-            className='w-40 h-60 rounded-md shadow-md object-cover object-center'
-            loading='lazy'
+            className='w-32 h-48 sm:w-40 sm:h-60 rounded-md shadow-md object-cover object-center'
+            loading='eager'
             width={160}
             height={240}
           />
@@ -78,9 +78,9 @@ const MostReadFocusCard = ({ item }: MostReadFocusCardProps) => {
       </div>
       <div className='text-base-content flex flex-col self-start text-left max-h-60'>
         <Link className='font-extrabold text-3xl' href={link}>
-          <h2 className='hover:underline'>{item.title}</h2>
+          <h2 className='hover:underline line-clamp-2'>{item.title}</h2>
         </Link>
-        <p className='text-sm overflow-hidden whitespace-break-spaces text-justify'>
+        <p className='text-sm whitespace-break-spaces text-justify line-clamp-5'>
           {item.description}
         </p>
       </div>
