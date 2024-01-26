225ed684d17699364da2e6d737dafbc14551fbc3
Matheus <matheussmoura@outlook.com>
2024-01-26 17:48:59 -0300

    changed focused most read to the first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,13 @@ export default async function Home() {
   const mostRead = await mangadex.mostRead();
   const releases = await mangadex.releases(1);
 
+  const [firstMostRead, ...mostReads] = mostRead ?? [];
+
   return (
     <div className='flex flex-col my-3'>
       <SearchBar />
       <FeaturedScroll />
-      <MostReadScroll items={mostRead} focused={mostRead.pop()!} />
+      <MostReadScroll items={mostReads} focused={firstMostRead} />
       <Releases releases={releases} />
       <NextPageButton />
     </div>
