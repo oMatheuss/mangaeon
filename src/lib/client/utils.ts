8bbc525384d1b0d033acfe942753171d298830b2
Matheus <matheussmoura@outlook.com>
2024-06-12 23:34:34 -0300

    removed unused functions

--- src/lib/client/utils.ts
+++ src/lib/client/utils.ts
@@ -1,31 +1,6 @@
 import type { Manga } from '@/types/manga';
 import type { MangaToSave } from './saved';
 
-export interface ErrorResponse {
-  status: number;
-  statusText: string;
-}
-
-export const toErrorReponse = (res: Response): ErrorResponse => {
-  return {
-    status: res.status,
-    statusText: res.statusText,
-  };
-};
-
-export const mapUntil = <T, S>(
-  arr: T[],
-  callback: (value: T, index: number, array: T[]) => S,
-  condFn: (value: T, index: number, array: T[]) => boolean
-) => {
-  const result: S[] = [];
-  for (let i = 0; i < arr.length; ++i) {
-    result.push(callback(arr[i], i, arr));
-    if (condFn(arr[i], i, arr)) break;
-  }
-  return result;
-};
-
 export const fromMangaToSaved = (manga: Manga) => {
   return {
     artist: manga.artist,
