d3e93dbdf5b438530e6a47ef95ecce925a01f93b
matheus <matheussmoura@outlook.com>
2025-01-28 20:50:03 -0300

    update next & error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,27 +1,27 @@
 'use client';
 
-export default function Error({
-  error,
-  reset,
-}: {
+import { Button } from "@/components/ui/button";
+import { RotateCcwIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface ErrorProps {
   error: Error & { digest?: string };
-  reset: () => void;
-}) {
+}
+
+export default function Error({ error }: ErrorProps) {
+  const { refresh } = useRouter();
   return (
     <div className='my-32 flex flex-col items-center text-center'>
       <h2 className='mb-3 text-xl font-bold tracking-tight text-red-500'>
-        500 - Internal Server Error
+        Internal Error
       </h2>
       <p className='mb-1'>
         {error.name} - {error.message}
       </p>
-      {error.digest && <p className='mb-1'>Digest: {error.digest}</p>}
-      <button
-        className='text-blue-600 after:content-["_↗"] dark:text-blue-400'
-        onClick={reset}
-      >
-        Tentar de Novo
-      </button>
+      {error.digest && <p className='mb-3'>Digest: {error.digest}</p>}
+      <Button onClick={refresh}>
+        <RotateCcwIcon/>Tentar de Novo
+      </Button>
     </div>
   );
 }
